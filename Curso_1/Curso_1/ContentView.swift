3c11528e82d3c274bcb34683aff82d00f02280de
Alejandro Gutiérrez Pérez <alejandro.gutierrez@grupocmc.es>
2021-05-22 17:45:41 +0200

    init proyect

---
+++ Curso_1/Curso_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Curso_1
+//
+//  Created by Alejandro Gutiérrez Pérez on 22/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Curso_1/Curso_1/Curso_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_1App.swift
+//  Curso_1
+//
+//  Created by Alejandro Gutiérrez Pérez on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Curso_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
