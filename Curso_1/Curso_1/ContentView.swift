ac240125ff672693b7b681bc48578b88e80b28e4
Alejandro Gutiérrez Pérez <alejandro.gutierrez@grupocmc.es>
2021-05-23 19:43:40 +0200

    seccion_3.1-Text

--- Curso_1/Curso_1/ContentView.swift
+++ Curso_1/Curso_1/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world!")
+            .fontWeight(.light)
+            .font(.custom("Palatino", size: 25))
+            .foregroundColor(.blue)
+            .multilineTextAlignment(.center)
+            .lineLimit(nil)
+            .truncationMode(.middle)
+            .lineSpacing(3)
+            .padding(8)
+//            .rotationEffect(.degrees(10), anchor: UnitPoint(x: 0, y: 0))
+            .rotation3DEffect(.degrees(30), axis: (x: 1, y: 0, z: 0))
+            .shadow(color: .orange, radius: 2, x: 0, y: 2)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
